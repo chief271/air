9403b7d1519d93540ac6bb181846270aeebe69f9
chief271 <mohamedtssh19@gmail.com>
2025-03-16 02:19:47 +0100

    Update db.js

--- db.js
+++ db.js
@@ -1,12 +1,11 @@
 const { Pool } = require("pg");
-const { Pool } = require("pg");
+
 
 const pool = new Pool({
   connectionString: "postgresql://postgres:lhsuKSVTHcDEpvPkTDUhXZgExLXEFJth@centerbeam.proxy.rlwy.net:26655/railway",
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  ssl: { rejectUnauthorized: false, require: true },
 });
+
 
 pool.connect()
   .then(() => console.log("✅ Connected to PostgreSQL on Railway"))
